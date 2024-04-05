744b95872c9ff98ec8e41944fa3e9ced68430976
LasiniPrabashi <lasiniprabashi0327@gmail.com>
2024-04-05 13:01:38 +0530

    item update

---
+++ POS_Back_End/src/main/java/lk/ijse/spring/entity/OrderItem.java
@@ -0,0 +1,15 @@
+package lk.ijse.spring.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class OrderItem implements Serializable {
+    private String oid;
+    private String itemCode;
+}
